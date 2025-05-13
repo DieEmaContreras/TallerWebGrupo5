10ee7f02d85d12f3227b84126e754da8d07ad6d2
Diego Contreras <166648552+DieEmaContreras@users.noreply.github.com>
2025-05-13 20:44:25 -0300

    Create Mascota.java

---
+++ src/main/java/com/tallerwebi/dominio/Mascota.java
@@ -0,0 +1,59 @@
+package com.tallerwebi.dominio;
+
+public class Mascota {
+
+    private int id;
+    private String nombre;
+    private String especie;
+    private String raza;
+    private int edad;
+    private String nombreDueño;
+
+    public int getId (){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNombre (){
+        return nombre;
+    }
+
+    public void setNombre (String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecie (){
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombreDueño() {
+        return nombreDueño;
+    }
+
+    public void setNombreDueño(String nombreDueño) {
+        this.nombreDueño = nombreDueño;
+    }
+}
